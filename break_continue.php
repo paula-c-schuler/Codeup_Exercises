fce83ab283da74e8fb38e5a340268693a40cab86
Paula C Schuler <paulasatx@gmail.com>
2015-03-11 14:56:53 -0500

    Used break to exit For Loop.

--- break_continue.php
+++ break_continue.php
@@ -3,7 +3,12 @@
 for ($i = 0; $i <= 100; $i += 2){
 	echo $i . PHP_EOL;
 }
-
-
+//Create another for loop that counts from 1 to 100, but stops after 10 using break.
+for ($i = 0; $i <= 100; $i++){
+	echo $i . PHP_EOL;
+	if ($i == 10){
+		break;
+	}
+}
 
 
