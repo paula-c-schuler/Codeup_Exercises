1ca9788b2bfe58be4942583718e1078385811c5b
Paula C Schuler <paulasatx@gmail.com>
2015-03-11 14:38:20 -0500

    Refined the code.

--- foreach_books.php
+++ foreach_books.php
@@ -33,6 +33,7 @@ $books = array(
 //then list the key value pairs for the data about each book.
 foreach ($books as $key => $book) {
 	echo $key . " is a book. " . PHP_EOL;
+
 	foreach ($book as $key2 => $value) {
 		echo "$key2 is $value. " . PHP_EOL;
 	}
@@ -41,12 +42,8 @@ foreach ($books as $key => $book) {
 
 //Update your loop to only show books that were written after 1950.
 foreach ($books as $key => $book) {
-	
-	foreach ($book as $key2 => $value) {
-
-		if ($book['published'] > 1950){
+	if ($book['published'] > 1950){
 			echo $key . PHP_EOL;
-		}
 	}
 	echo "======================\n";
 }
