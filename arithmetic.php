7eee1403eaab2f06b578a67da650d5ee094436f1
Paula C Schuler <paulasatx@gmail.com>
2015-03-12 09:21:56 -0500

    PHP Functions: Working with arithmetic, first commit.

---
+++ arithmetic.php
@@ -0,0 +1,42 @@
+<?php
+//************ EXERCISES WITH ARITHMETIC *************
+//Fill in the // Add code here blocks to make each function echo the proper result.
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    // Add code here
+}
+
+function multiply($a, $b) {
+    // Add code here
+}
+
+function divide($a, $b) {
+    // Add code here
+}
+
+//Fill in the // Add code here blocks to make each function echo the proper result.
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    ECHO $b - $a . PHP_EOL;
+}
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+function divide($a, $b) {
+    echo $b / $a . PHP_EOL;
+}
+//Add code after functions that calls each function with real numbers.
+
+
+
+
+
+?>
