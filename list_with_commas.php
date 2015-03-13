ef28fe4b275e860ac79d94684a7e84a7f5713ab6
Paula C Schuler <paulasatx@gmail.com>
2015-03-13 17:15:23 -0500

    Completed and working: Imploding, exploding, restringing for English output.

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,21 +1,31 @@
 <?php
-echo "Hello" . PHP_EOL;
 // List of famous peeps
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
  // TODO: Convert the string into an array
 
 $physicistsArray = explode(', ', $physicistsString);
-
-var_dump($physicistsArray);
-function humanizedList($physicistsArray) {
-	$lastPhysicist = array_pop($physicistsArray);
-	var_dump($lastPhysicist);
-	$newArrayPhysicists = implode(', ', $physicistsArray);
-	$famousFakePhysicists = $newArrayPhysicists . ", and " . $lastPhysicist . PHP_EOL;
-	echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}. " . PHP_EOL;
-}
-humanizedList($physicistsArray);
+$passedArray = $physicistsArray;
 
 
+function humanizedList($passedArray, $sort = false) {
+	//alphabetize  by first names, then do the rest 
+	//additional exercise makes this optional based on second argument true
+	if ($sort) {
+		asort($passedArray);
+	} 
+	//adjusts array to add proper "and" at end of the sentence required by criteria.
+	$itemRemoved = array_pop($passedArray);
+	$newStringOfItems = implode(', ', $passedArray);
+	$humanizedString = $newStringOfItems . ", and " . $itemRemoved . PHP_EOL;
+	return $humanizedString;
+} 
 
-
+$sort = true;
+//add second argument if want to sort alphabetically,
+//call the function above, assign value to new variable for English output. 
+$famousFakePhysicists = humanizedList($physicistsArray, $sort = true);
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}. " . PHP_EOL;
+//********** NOTE TO SELF -- RETURN is necessary to pull result from function. ****
+//********** However, variable names can be crazy random. ****
+//********** $famousFakePhysicists pulls the return because ****
+//********* it is assigned the value of the entire function, which is the RETURN. ****
