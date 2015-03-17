81e1233f87598b4368aa66422042690a3d3b01a1
Paula C Schuler <paulasatx@gmail.com>
2015-03-17 05:00:30 -0500

    if_and_or.php completed. PHP I curriculum.

--- if_and_or.php
+++ if_and_or.php
@@ -13,25 +13,25 @@ if ($x > 0 || $x > 10){
 }
 // echo is    _________
 if ($y > 0 || $y > 10){
-	echo "{$y} is greater than 0 or {$y} is greater than 10\n";
+	echo "{$y} > 0 or {$y} > 10\n";
 }
-// echo is   5 > 0 or 5 > 10
 
+// echo is   5 > 0 or 5 > 10
 if ($z > 0 && $z < 10){
 	echo "{$z} > 0 and {$z} < 10\n";
 }
-// echo is    ____________
 
+// echo is    ____________
 if ($x > 0 && $x < 10){
 	echo "{$x} > 0 and {$x} < 10\n";
 }
-// echo is   ________
 
+// echo is   ________
 if ($y > 0 && $y < 10){
 	echo "{$y} > 0 and {$y} < 10\n";
 }
-// 5 > 0 and 5 < 10
 
+// 5 > 0 and 5 < 10
 if ($z > 0 && $z < 10){
 	echo "{$z} > 0 and {$z} < 10\n";
 }
