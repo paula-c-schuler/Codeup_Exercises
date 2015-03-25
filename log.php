33d3bb62d763bac91650e38d6d5eedd59d63bad6
Paula C Schuler <paulasatx@gmail.com>
2015-03-25 13:33:54 -0500

    Finished 7.3.1 log.php and lot_test.php working.

--- log.php
+++ log.php
@@ -1,27 +1,52 @@
 <?php  
 // *** EXERCISE 7.3 COMPLETED, WORKING ***
+// *** EXERCISE 7.3.1  COMPLETED  CONSTRUCTORS/DESTRUCTORS,   ***
 class Log 
 {
-	public $filename = '';
+	public $filename = '$prefix-YYYY-MM-DD.log';
+	public $handle;
 
-	public function logMessage($logLevel, $message) {
+	// constructor is first function, good practice
+	public function __construct ($prefix = 'log') 
+	// $prefix = 'log' is how a default is set for a function,
+	// if a parameter is not actually passed for $prefix.
+	{
+		date_default_timezone_set('America/Chicago');
+		$this->filename = $prefix . ' ' . date("Y-m-d-s") . '-.log';
+		$this->handle = fopen($this->filename, 'a+');
+	}
+
+
+	public function logMessage($logLevel, $message) 
+	{
+
 		$fileDate = date('Y-m-d');
 		$time = date('h:i:s');
 		// var_dump($fileDate);
-	    // $filename = 'log-' . $fileDate . '.txt';
+	    // $filename = 'log-' . $fileDate . '.txt'; *** ACTION MOVED TO __construct
 	    
-		$handle = fopen($this->filename, 'a+');
-		
-	    fwrite($handle, PHP_EOL . $fileDate . ' ' . $time . ' [' . $logLevel . '], ' . $message);
-		fclose($handle);
+		// $handle = fopen($this->filename, 'a+'); *** ACTION MOVED TO __construct
+		var_dump($this->handle);
+	    fwrite($this->handle, PHP_EOL . $fileDate . ' ' . $time . ' [' . $logLevel . '], ' . $message);
+		// fclose($handle); *** ACTION MOVED TO __destruct
 	}
 
-	public function info() {
+	
+
+	public function info() 
+	{
 		$this->logMessage("INFO", "This is an info message.");
 	}
 
-	public function error() {
+	public function error() 
+	{
 		$this->logMessage("ERROR", "This is an info message.");
+	}
+
+	public function __destruct()
+	{
+		fclose($this->handle);
+		echo ("This is closed.");
 	}
 
 }
--- log_test.php
+++ log_test.php
@@ -1,14 +1,15 @@
 <?php 
 // *** EXERCISE 7.3 COMPLETED, WORKING ***
+// *** EXERCISE 7.3.1 COMPLETED CONSTRUCTORS/DECONSTRUCTORS     ***
 require_once 'Log.php';
 
-$log = new Log();
+$log = new Log('cli');
 	
 // NTS: This assignment of filename applies to this
 // instance of Log class only, not to any other instances.
-$log->filename = 'log-YYYY-MM-DD-log.php';
+// $log->filename = 'log-YYYY-MM-DD-log.php'; ACTION TRANSFERRED TO __construct IN LOG.PHP
 
-echo $log->logMessage('freakyFriday', 'Howdy Friday') . PHP_EOL;
-echo $log->info();
+// echo $log->logMessage() . PHP_EOL;÷
+echo $log->info('All is well.');
 echo $log->error();
 ?>
